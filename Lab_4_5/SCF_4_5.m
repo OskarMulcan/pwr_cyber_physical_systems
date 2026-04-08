6c1911d4e68e76d4ecd38da0db9c4d6e078e120f
iwonieevo <iwo.chwiszczuk@gmail.com>
2026-04-08 10:40:45 +0200

    portrety fazowe a-g

--- Lab_4_5/SCF_4_5.m
+++ Lab_4_5/SCF_4_5.m
@@ -26,6 +26,14 @@ plot(zad2.t, zad2.x(:,1));
 
 % Zadanie #3
 function zad3_phase_portrait(A, T, quiver_range_x1, quiver_range_x2, traj_range_x1, traj_range_x2)
+    arguments
+        A               (2,2) {mustBeNumeric}
+        T               (1,1) {mustBeNumeric, mustBePositive}
+        quiver_range_x1 (1,:) {mustBeNumeric}
+        quiver_range_x2 (1,:) {mustBeNumeric}                  = quiver_range_x1
+        traj_range_x1   (1,:) {mustBeNumeric}                  = quiver_range_x1
+        traj_range_x2   (1,:) {mustBeNumeric}                  = quiver_range_x2
+    end
     dot_x = @(t, x) A * x;
     t = 0:0.01:T;
 
@@ -46,7 +54,47 @@ function zad3_phase_portrait(A, T, quiver_range_x1, quiver_range_x2, traj_range_
     xlim([min(x_0(:,1)) max(x_0(:,1))]);
     ylim([min(x_0(:,2)) max(x_0(:,2))]);
     xlabel('x_1'); ylabel('x_2');
+    title(sprintf(['A=', mat2str(A)]));
     grid on;
 end
 
-zad3_phase_portrait([-1 4; -2 5], 1, -3:1:3, -3:1:3, -1:1:1, -1:1:1)
+%a)
+A = [-1 4;
+     -2 5];
+zad3_phase_portrait(A, 1, -2.8:0.4:2.8)
+
+%b)
+A = [-3  0;
+      0 -2];
+zad3_phase_portrait(A, 2, -8:2:8)
+
+%c)
+A = [4  0;
+     2 -1];
+zad3_phase_portrait(A, 1, -2.8:0.4:2.8)
+
+%d)
+A = [2   -3;
+     1/3  4];
+zad3_phase_portrait(A, 1, -2.8:0.4:2.8)
+
+%e)
+A = [-7  1;
+     -4 -3];
+zad3_phase_portrait(A, 2, -8:2:8)
+
+%f)
+A = [-2  3;
+     -3 -2];
+zad3_phase_portrait(A, 2, -8:2:8)
+
+%g)
+A = [ 2  3;
+     -3  2];
+zad3_phase_portrait(A, 1, -2.8:0.4:2.8)
+
+%h)
+A = [ 0  1;
+     -5  0];
+zad3_phase_portrait(A, 2, -8:2:8)
+
