03155b20f90d68ac06d5fe792199bcff8db2c8a6
iwonieevo <iwo.chwiszczuk@gmail.com>
2026-04-08 12:42:40 +0200

    analiza typu punktu równowagi i stabilności na podstawie macierzy A

--- Lab_4_5/SCF_4_5.m
+++ Lab_4_5/SCF_4_5.m
@@ -58,43 +58,117 @@ function zad3_phase_portrait(A, T, quiver_range_x1, quiver_range_x2, traj_range_
     grid on;
 end
 
+function zad3_analysis(A)
+    arguments
+        A (2,2) {mustBeNumeric}
+    end
+    tau = trace(A);
+    delta = det(A);
+    discriminant = tau^2 - 4*delta;
+
+    fprintf("A=%s\n", mat2str(A))
+    fprintf("trace(A)=%.2f\n", tau)
+    fprintf("det(A)=%.2f\n", delta)
+
+    % Klasyfikacja wg Strogatza (Rozdział 5.2)    
+    if delta < 0
+        type = 'Siodło';
+        stability = 'Niestabilny';
+        
+    elseif delta > 0
+        if discriminant > 0
+            type = 'Węzeł';
+
+        elseif discriminant < 0
+            type = 'Ognisko';
+
+        else % discriminant == 0
+            if isdiag(A) && A(1,1) == A(2,2)
+                type = 'Gwiazda';
+            else
+                type = 'Węzeł zdegenerowany';
+            end
+        end
+
+        if tau > 0
+            stability = 'Niestabilny';
+
+        elseif tau < 0
+            stability = 'Stabilny (asymptotycznie)';
+
+        else
+            type = 'Centrum';
+            stability = 'Stabilny (w sensie Lapunova)';
+        end
+
+    else % delta == 0
+        if A == 0
+            type = 'Płaszczyzna punktów równowagi';
+
+        else
+            type = 'Linia punktów równowagi';
+        end
+
+        stability = 'Niestabilny lub stabilny (w sensie Lapunova)';
+    end
+
+    fprintf('Typ punktu równowagi: %s\n', type)
+    fprintf('Stabilność: %s\n\n', stability)
+end
+
 %a)
 A = [-1 4;
      -2 5];
+fprintf("a)\n")
+zad3_analysis(A)
 zad3_phase_portrait(A, 1, -2.8:0.4:2.8)
 
 %b)
 A = [-3  0;
       0 -2];
+fprintf("b)\n")
+zad3_analysis(A)
 zad3_phase_portrait(A, 2, -8:2:8)
 
 %c)
 A = [4  0;
      2 -1];
+fprintf("c)\n")
+zad3_analysis(A)
 zad3_phase_portrait(A, 1, -2.8:0.4:2.8)
 
 %d)
 A = [2   -3;
      1/3  4];
+fprintf("d)\n")
+zad3_analysis(A)
 zad3_phase_portrait(A, 1, -2.8:0.4:2.8)
 
 %e)
 A = [-7  1;
      -4 -3];
+fprintf("e)\n")
+zad3_analysis(A)
 zad3_phase_portrait(A, 2, -8:2:8)
 
 %f)
 A = [-2  3;
      -3 -2];
+fprintf("f)\n")
+zad3_analysis(A)
 zad3_phase_portrait(A, 2, -8:2:8)
 
 %g)
 A = [ 2  3;
      -3  2];
+fprintf("g)\n")
+zad3_analysis(A)
 zad3_phase_portrait(A, 1, -2.8:0.4:2.8)
 
 %h)
 A = [ 0  1;
      -5  0];
+fprintf("h)\n")
+zad3_analysis(A)
 zad3_phase_portrait(A, 2, -8:2:8)
 
